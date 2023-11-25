1fc1c6bf8de4487bb47d0c600326e583c70e7bed
ruben170305 <ruben17032005@gmail.com>
2023-11-25 21:25:34 +0100

    initial commit

---
+++ src/v1/Jugador.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package v1;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Jugador {
+
+	private String name;
+
+	private int[] puntuacion = new int[PartidaDardos.numeroDeRondas];
+
+	private String[] resultadosPartida = new String[PartidaDardos.numeroDeRondas];
+
+	public Jugador(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void jugarTurno(int i) {
+		Random random = new Random();
+
+		System.out.println("Turno de " + name + ". Lanza un dardo.");
+
+		int puntuacion = random.nextInt(20) + 1;
+		int multiplicador = random.nextInt(6) + 1;
+
+		switch (multiplicador) {
+		case 1, 2, 3, 4:
+			puntuacion = puntuacion * 1;
+			resultadosPartida[i] = "-";
+			break;
+		case 5:
+			puntuacion = 25;
+			resultadosPartida[i] = "D";
+			break;
+		case 6:
+			puntuacion = 50;
+			resultadosPartida[i] = "D";
+			break;
+		case 0:
+			puntuacion = puntuacion * 0;
+			resultadosPartida[i] = "-";
+			break;
+		default:
+			break;
+		}
+		System.out.println("La puntuacion obtenida es: " + puntuacion);
+		this.puntuacion[i] = puntuacion;
+	}
+
+	public int calcularPuntuacion() {
+		int suma = 0;
+		for (int i = 0; i < puntuacion.length; i++) {
+			suma = suma + puntuacion[i];
+		}
+		return suma;
+	}
+
+	public void mostrarPuntuacion() {
+		System.out.println("\nPuntuacion de "+name);
+		System.out.println(Arrays.toString(puntuacion));
+		System.out.println(Arrays.toString(resultadosPartida));
+		System.out.println("La puntuación total de "+name+" es: "+calcularPuntuacion());
+	}
+	
+	public int calcularDianas() {
+		int dianas = 0;
+		for (int i = 0; i < resultadosPartida.length; i++) {
+			if(resultadosPartida[i] == "D") {
+				dianas++;
+			}
+		}
+		return dianas;
+	}
+
+}
---
+++ src/v1/PartidaDardos.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package v1;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class PartidaDardos {
+	
+	private static int puntuacionRecord;
+	private static String nombreJugadorRecord;
+	
+	public static void comprobarRecord(int puntuacionActual, String name) {
+		System.out.println("\n¡RECORD DE PUNTOS!");
+		if (puntuacionRecord < puntuacionActual) {
+			System.out.println("Se ha superado el record registrado.\nActualmente era de "+puntuacionRecord+" y pertecia a "+nombreJugadorRecord);
+			puntuacionRecord = puntuacionActual;
+			nombreJugadorRecord = name;
+			System.out.println("El nuevo record es: "+puntuacionRecord+"\nEl record lo ha superado el jugador "+nombreJugadorRecord);
+		}
+	}
+	
+	private static int recordDianas;
+	private static String jugadorRecordDianas;
+	
+	public static void comprobarNDianas(int NDianas, String name) {
+		System.out.println("\n¡RECORD DE DIANAS!");
+		if (recordDianas < NDianas) {
+			System.out.println("Se ha superado el record registrado.\nActualmente era de "+recordDianas+" y pertecia a "+jugadorRecordDianas);
+			recordDianas = NDianas;
+			jugadorRecordDianas = name;
+			System.out.println("El nuevo record es: "+recordDianas+"\nEl record lo ha superado el jugador "+jugadorRecordDianas);
+		}
+	}
+	
+	static int numeroDeRondas = 10;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Scanner reiniciar = new Scanner(System.in);
+		String reinicio="";
+		do {
+		System.out.println("Introduce tu nombre:");
+		String name = sc.nextLine();
+		Jugador player1 = new Jugador(name);
+		System.out.println("Introduce tu nombre:");
+		name = sc.nextLine();
+		Jugador player2 = new Jugador(name);
+		sc.close();
+		
+		for (int i = 0; i < numeroDeRondas; i++) {
+			player1.jugarTurno(i);
+			player2.jugarTurno(i);
+		}
+		player1.mostrarPuntuacion();
+		player2.mostrarPuntuacion();
+		
+		//COMPROBAMOS QUIEN HA GANADO
+		int puntuacion1 = player1.calcularPuntuacion();
+		int puntuacion2 = player2.calcularPuntuacion();
+		System.out.println("\n");
+		if (puntuacion1 > puntuacion2) {
+			System.out.println("Ha ganado: "+player1.getName());
+			comprobarRecord(puntuacion1, player1.getName());
+		} else if (puntuacion1 < puntuacion2) {
+			System.out.println("Ha ganado: "+player2.getName());
+			comprobarRecord(puntuacion2, player2.getName());
+		} else {
+			System.out.println("Los dos jugadores han conseguido la misma puntuación");
+			comprobarRecord(puntuacion1, player1.getName()+" y "+player2.getName());
+		}
+		
+		//COMPROBAMOS EL NUMERO DE DIANAS
+		System.out.println("\n");
+		int dianasP1 = player1.calcularDianas();
+		int dianasP2 = player2.calcularDianas();
+		
+		if (dianasP1 > dianasP2) {
+			System.out.println("Ha dado más veces a la diana: "+player1.getName());
+			comprobarNDianas(dianasP1, player1.getName());
+		} else if (dianasP1 < dianasP2) {
+			System.out.println("Ha dado más veces a la diana: "+player2.getName());
+			comprobarNDianas(dianasP2, player2.getName());
+		} else {
+			System.out.println("Han empatado en el numero de dianas los dos jugadores");
+			comprobarNDianas(dianasP1, player1.getName()+" y "+player2.getName());
+		}
+		
+		System.out.print("\n¿Quieres reiniciar la partida? ");
+		reinicio = reiniciar.nextLine();
+		} while(reinicio == "si");
+
+	}
+		
+	
+	
+
+}
